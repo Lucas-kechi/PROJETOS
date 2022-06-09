4003119ffab4275e593a56fca16f68b8b3d174e6
A81435269g <lukaskechi7@gmail.com>
2022-06-09 16:18:42 -0300

    Aulas Rocketseat

---
+++ Rocketseat/JavaScript/tipos-de-dados.js
@@ -0,0 +1,8 @@
+//Dados de string//
+//Jeitos corretos de escrita//
+console.log("Lucas")
+console.log("Luca's")
+console.log('Lucas')
+console.log('Luca"s')
+console.log(`Lu"ca's`)
+console.log(`Lucas ${1+9+9+5+0+7+2+4}`)
