bd582c07e31a3502096b048a5a079b20f625a45f
A81435269g <lukaskechi7@gmail.com>
2022-06-09 16:37:22 -0300

    Aula de number

--- Rocketseat/JavaScript/tipos-de-dados.js
+++ Rocketseat/JavaScript/tipos-de-dados.js
@@ -1,8 +1,21 @@
-//Dados de string//
-//Jeitos corretos de escrita//
+/*Dados de string
+Jeitos corretos de escrita*/
 console.log("Lucas")
 console.log("Luca's")
 console.log('Lucas')
 console.log('Luca"s')
 console.log(`Lu"ca's`)
 console.log(`Lucas ${1+9+9+5+0+7+2+4}`)
+
+/*Number 
+aqui veremos exemplos de number*/
+//Inteiro
+console.log(25)
+//Real
+console.log(12.5)
+//Expressão soma(+) subtração(-) Divisão(/) Multiplicação(*)
+console.log(25/12.5)
+//NaN, Podemos obter esse number quando fizermos por exemplo um expressão com uma string
+console.log(2*"lucas")
+//Infinity, tem que ser escrito com a letra "i" em maiusculo "I", sem isso dará erro
+console.log(Infinity+2)
