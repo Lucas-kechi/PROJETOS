da99c70044b34458728bf3a7680f872df2665198
A81435269g <lukaskechi7@gmail.com>
2022-07-19 16:04:29 -0300

    Condicionais e controle de fluxo

---
+++ Rocketseat/JavaScript/condicionais_e_controle_de_fluxo.js
@@ -0,0 +1,16 @@
+//  CONDICIONAIS E CONTROLE DE FLUXO
+{
+    /*  CONTROLE DE FLUXO DA APLICAÇÃO
+	Nas próximas aulas falaremos sobre o controle do fluxo da nossa aplicação. Temos um padrão de fluxo de leitura da linguagem que é de forma 
+    ordenada de cima para baixo. Mas agora aprenderemos a pular blocos de códigos se determinadas condições forem cumpridas. 
+    Exemplo:*/
+	// Vou tomar café 
+	//Se meu copo estiver sujo então
+	console.log('Limpar copo');
+	// se não
+	console.log('Servir café');
+    /*
+    Vamos praticar e aprender como realizar essas condições para que determinados blocos de códigos sejam executados, assim controlando o fluxo e 
+    criando um algoritmo para minha aplicação.
+    */
+}
