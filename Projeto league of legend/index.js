665f8ec63097d8bf372050596cc7edafa75f8911
A81435269g <lukaskechi7@gmail.com>
2022-08-31 14:28:56 -0300

    Upgrade

--- Projeto league of legend/index.js
+++ Projeto league of legend/index.js
@@ -4,7 +4,10 @@ const {userName, userPassword} = fakeUser;
 const user = document.querySelector("[name=userName]");
 const password = document.querySelector("[name=userPassword]");
 const loginButton = document.querySelector('[name=loginButton]');
-const body = document.querySelector('body');
+const logo = document.querySelector('#logoSection');
+const facebookButton = document.querySelector('#facebookButton')
+const googleButton = document.querySelector('#googleButton')
+const appleButton = document.querySelector('#appleButton')
 let newUser;
 let newPassword;
 
@@ -15,18 +18,26 @@ function getValueFromInput(event) {
         newPassword = event.currentTarget.value;
     };
 };
+
 function toggleDisabled() {
     if(newUser && newPassword) {
-        loginButton.removeAttribute('disabled')
+        loginButton.removeAttribute('disabled');
+        loginButton.classList.add('active-button')
     } else {
-        loginButton.setAttribute('disabled', 'disabled')
+        loginButton.setAttribute('disabled', 'disabled');
+        loginButton.classList.remove("active-button")
     }
 };
 
-user.addEventListener('keyup', getValueFromInput);
-user.addEventListener('mouseout', toggleDisabled);
-password.addEventListener('keyup', getValueFromInput);
-password.addEventListener('mouseout', toggleDisabled);
+user.addEventListener('keyup', (event) => {
+    getValueFromInput(event); 
+    toggleDisabled();
+});
+
+password.addEventListener('keyup', (event) => {
+    getValueFromInput(event);
+    toggleDisabled();
+});
 
 loginButton.addEventListener('click', () => {
     if(newUser === userName && newPassword === userPassword){
@@ -35,3 +46,9 @@ loginButton.addEventListener('click', () => {
         return alert('Usuário ou senha incorreto')
     };
 });
+
+logo.addEventListener('click', () => document.location.assign('https://www.leagueoflegends.com/pt-br/'))
+facebookButton.addEventListener('click', () => document.location.assign('https://www.facebook.com'))
+googleButton.addEventListener('click', () => document.location.assign('https://accounts.google.com/'))
+appleButton.addEventListener('click', () => document.location.assign('https://appleid.apple.com/'))
+
