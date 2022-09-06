49babfd754f540bb53c8c53c404820c8544dcbc3
A81435269g <lukaskechi7@gmail.com>
2022-09-05 23:05:13 -0300

    Update

--- Project getColor and setColor/script.js
+++ Project getColor and setColor/script.js
@@ -1,8 +1,10 @@
 const spansFromPainel = document.querySelectorAll('#colorsBorder span');
 const spansFromGrid = document.querySelectorAll('#grid span');
 const grid = document.querySelector('#grid');
+const painel = document.querySelector('#colorsBorder')
 const clearGridButton = document.querySelector('button');
-const gridInput = document.querySelector('input');
+const gridInput = document.querySelector('#dimensionInput');
+const buttonSetSpanFromPainel = document.querySelector('#colorsBorder button')
 let setColor;
 let gridConfig;
 
@@ -13,15 +15,20 @@ spansFromGrid.forEach(el => el.addEventListener('click', () => el.classList.add(
 clearGridButton.addEventListener('click', () => spansFromGrid.forEach(el => el.removeAttribute('class')));
 
 gridInput.addEventListener('keyup', (event) => {
-    const span = document.createElement('span')
+    
+});
 
-    gridConfig = event.currentTarget.value ** 2;
+buttonSetSpanFromPainel.addEventListener('click', () => {
+    const inputColor = document.createElement('input');
+    
+    inputColor.setAttribute('type', 'color');
+    inputColor.setAttribute('style', 'margin-top: 3rem');
+    painel.append(inputColor);
 
-    if(grid.children.length !== 0) {
-        console.log(grid.childNodes)
-        grid.childNodes.forEach(el => grid.removeChild('span'));
-    };
-
-   grid.prepend(span)
-
+    inputColor.addEventListener('mouseleave', (event) => {
+        const span = document.createElement('span');
+        span.setAttribute('style', `background-color: ${inputColor.value}`);
+        painel.insertBefore(span, buttonSetSpanFromPainel);
+        painel.removeChild(painel.lastElementChild);
+    });
 });
