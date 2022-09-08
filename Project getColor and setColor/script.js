191c594acfb16345cbff57608e6d9728336cb06f
A81435269g <lukaskechi7@gmail.com>
2022-09-08 12:39:21 -0300

    Finishing project

--- Project getColor and setColor/script.js
+++ Project getColor and setColor/script.js
@@ -1,22 +1,14 @@
-const spansFromPainel = document.querySelectorAll('#colorsBorder span');
-const spansFromGrid = document.querySelectorAll('#grid span');
 const grid = document.querySelector('#grid');
-const painel = document.querySelector('#colorsBorder')
-const clearGridButton = document.querySelector('button');
+const painel = document.querySelector('#colorsBorder');
+const clearGridButton = document.querySelector('#buttonClearColorGrid');
 const gridInput = document.querySelector('#dimensionInput');
-const buttonSetSpanFromPainel = document.querySelector('#colorsBorder button')
-let setColor;
+const buttonSetSpanFromPainel = document.querySelector('#colorsBorder button');
+const buttonConfigGrid = document.querySelector('#enterGrid');
+let flexBasisValue;
+let spansFromGrid = document.querySelectorAll('#grid span');
+let spansFromPainel = document.querySelectorAll('#colorsBorder span');
+let setColor = 'background-color: #000000';
 let gridConfig;
-
-spansFromPainel.forEach(el => el.addEventListener('click', (event) => setColor = event.currentTarget.classList.value));
-
-spansFromGrid.forEach(el => el.addEventListener('click', () => el.classList.add(setColor)));
-
-clearGridButton.addEventListener('click', () => spansFromGrid.forEach(el => el.removeAttribute('class')));
-
-gridInput.addEventListener('keyup', (event) => {
-    
-});
 
 buttonSetSpanFromPainel.addEventListener('click', () => {
     const inputColor = document.createElement('input');
@@ -27,8 +19,44 @@ buttonSetSpanFromPainel.addEventListener('click', () => {
 
     inputColor.addEventListener('mouseleave', (event) => {
         const span = document.createElement('span');
+        
         span.setAttribute('style', `background-color: ${inputColor.value}`);
         painel.insertBefore(span, buttonSetSpanFromPainel);
         painel.removeChild(painel.lastElementChild);
+        spansFromPainel = document.querySelectorAll('#colorsBorder span')
+        
+        spansFromPainel.forEach(el => el.addEventListener('click', (event) => setColor = el.getAttribute('style')));
     });
+});
+
+clearGridButton.addEventListener('click', () => {
+    spansFromGrid.forEach(el => el.setAttribute('style', `flex-basis: ${flexBasisValue}px; background-color: #ffffff;`));
+
+    setColor = 'background-color: #000000';
+});
+
+gridInput.addEventListener('keyup', (event) => {
+    if(event.currentTarget.value ){
+        buttonConfigGrid.removeAttribute('disabled') ;
+    } else {
+        buttonConfigGrid.setAttribute('disabled', 'disabled');
+
+        for(let repeat = 0; repeat < gridConfig; ++repeat) {
+            grid.removeChild(grid.firstChild);
+        };
+    };
+});
+
+buttonConfigGrid.addEventListener('click', () => {
+    flexBasisValue = 1450 / gridInput.value;
+
+    gridConfig = gridInput.value **2;
+
+    for(let repeat = 0; repeat < gridConfig; ++repeat) {
+        grid.prepend(document.createElement('span'));
+    };
+
+    spansFromGrid = document.querySelectorAll('#grid span');
+    spansFromGrid.forEach(el => el.addEventListener('click', () => el.setAttribute('style', `flex-basis: ${flexBasisValue}px; ${setColor}` )));
+    spansFromGrid.forEach(el => el.setAttribute('style', `flex-basis: ${flexBasisValue}px`));
 });
