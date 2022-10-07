15755693dd468c5caf50833cae68700ea16821bf
A81435269g <lukaskechi7@gmail.com>
2022-10-07 14:44:18 -0300

    New Verbs post, put and delete

--- Rocketseat/fetch-discover-API/main.js
+++ Rocketseat/fetch-discover-API/main.js
@@ -1,4 +1,14 @@
 const url = 'http://localhost:5500/api';
+const newUser = {
+    'name': 'Lucas Kechi',
+    'avatar': 'https://scontent.fsdu37-1.fna.fbcdn.net/v/t1.6435-9/51798043_1857005967743414_4321007047060488192_n.jpg?_nc_cat=103&ccb=1-7&_nc_sid=09cbfe&_nc_ohc=g5irum9igHkAX9InNHX&_nc_ht=scontent.fsdu37-1.fna&oh=00_AT_7lfe2N_meYU-FjiSKKXcesl8O_XutI2aYxW-e5aVZiQ&oe=636347ED',
+    'city': 'Rio de Janeiro'
+};
+const updateUser = {
+    'name': 'Lucas de Freitas Kechi Paiva',
+    'avatar': 'https://scontent.fsdu37-1.fna.fbcdn.net/v/t1.6435-9/51798043_1857005967743414_4321007047060488192_n.jpg?_nc_cat=103&ccb=1-7&_nc_sid=09cbfe&_nc_ohc=g5irum9igHkAX9InNHX&_nc_ht=scontent.fsdu37-1.fna&oh=00_AT_7lfe2N_meYU-FjiSKKXcesl8O_XutI2aYxW-e5aVZiQ&oe=636347ED',
+    'city': 'Rio de Janeiro, Petrópolis'
+};
 
 async function fetchAPI() {
     await fetch(url)
@@ -7,8 +17,8 @@ async function fetchAPI() {
         .catch(error => console.error(error))
 };
 
-async function apiUser() {
-    await fetch(`${url}/1`)
+async function apiUser(id) {
+    await fetch(`${url}/${id}`)
         .then(result => result.json())
         .then(data => {
             setName.textContent = data.name;
@@ -18,5 +28,23 @@ async function apiUser() {
         .catch(error => console.error(error))
 };
 
+async function postNewUser() {
+    await fetch(url, {
+        'method': 'post',
+        'body': JSON.stringify(newUser),
+        'headers': {"content-type": "application/json; charset=UTF-8"}
+    })
+};
+
+async function putUser(id) {
+    await fetch(`${url}/${id}`, {
+        'method': 'put',
+        'body': JSON.stringify(updateUser),
+        'headers': {"content-type": "application/json; charset=UTF-8"}
+    })
+};
+
+// postNewUser()
+putUser(2)
 fetchAPI()
-apiUser()
+apiUser(2)
