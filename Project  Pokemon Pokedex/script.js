7bc7856b509589fde28ca913b629caf6fd4b2e1e
A81435269g <lukaskechi7@gmail.com>
2022-10-08 15:28:30 -0300

    update

--- Project  Pokemon Pokedex/script.js
+++ Project  Pokemon Pokedex/script.js
@@ -1,5 +1,21 @@
 window.onload = () => {
     soundTrack.autoplay = true;
+    soundOff.style = 'display: none'
     console.log(soundTrack);
 };
 
+const btnAudio = document.querySelector('#btnAudio')
+
+console.log(btnAudio)
+
+btnAudio.addEventListener('click', () => {
+    if(soundTrack.muted === false){
+        soundTrack.muted = true;
+        soundOn.style = "display: none";
+        soundOff.style = "display: flex";
+    } else {
+        soundTrack.muted = false;
+        soundOn.style = "display: flex";
+        soundOff.style = "display: none";
+    };
+});
